036e76ad7b13d8f95529a8f679e704f3df15c82f
sacap01 <sycap01@gmail.com>
2022-07-18 14:23:05 +0900

    chapter2. properties 외부설정사용법

--- src/main/java/com/example/fastcampuspractice/FastcampusPracticeApplication.java
+++ src/main/java/com/example/fastcampuspractice/FastcampusPracticeApplication.java
@@ -2,7 +2,9 @@ package com.example.fastcampuspractice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
+@ConfigurationPropertiesScan//환경변수를 따로 빼놓았으니 읽거라
 @SpringBootApplication
 public class FastcampusPracticeApplication {
 
